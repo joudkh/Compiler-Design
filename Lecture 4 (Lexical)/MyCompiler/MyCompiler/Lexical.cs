9d62b1d4b0b3504cffde4778ecdd25f4c588c7b3
Joud Khattab <khattab.joud@gmail.com>
2019-07-03 01:26:07 +0300

    Add files via upload

---
+++ Lecture 4 (Lexical)/MyCompiler/MyCompiler/Lexical.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCompiler
+{
+    public class Lexical
+    {
+        // list of keywords
+        // list of datatype
+        List<Token> tokenList;
+        int lineNumber = 1;
+
+        public Lexical()
+        {
+            tokenList = new List<Token>();
+        }
+
+        public void findTokens(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                if (Char.IsLetter(ch))
+                {
+                    string tmp = "";
+                    int tmpIndex = i;
+                    while (Char.IsLetter(ch) || Char.IsDigit(ch))
+                    {
+                        tmp += ch;
+                        tmpIndex++;
+                        if (tmpIndex < str.Length)
+                        {
+                            ch = str[tmpIndex];
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+                    //Console.WriteLine(tmp);
+                    // if keyword or datatype or id
+                    Token tmpToken = new Token(Language.datatype, tmp, lineNumber);
+                    tokenList.Add(tmpToken);
+                    i = tmpIndex - 1;
+                }
+                else if (Char.IsDigit(ch))
+                {
+                    // int
+                    // double
+                    string tmp = "";
+                    int tmpIndex = i;
+                    while (Char.IsDigit(ch))
+                    {
+                        tmp += ch;
+                        tmpIndex++;
+                        if (tmpIndex < str.Length)
+                        {
+                            ch = str[tmpIndex];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    Token tmpToken = new Token(Language.digit, tmp, lineNumber);
+                    tokenList.Add(tmpToken);
+                    i = tmpIndex - 1;
+
+                }
+                else if (ch == '=' || ch == ';' || ch == ',')
+                {
+                    string tmp = "";
+                    tmp += ch;
+                    Token tmpToken = new Token(Language.symbol, tmp, lineNumber);
+                    tokenList.Add(tmpToken);
+                }
+                else if(ch=='\n')
+                {
+                    lineNumber++;
+                }
+            }
+        }
+
+        public void printTokenList()
+        {
+            for (int i = 0; i < tokenList.Count; i++)
+            {
+                tokenList[i].printToken();
+            }
+        }
+
+    }
+}
---
+++ Lecture 4 (Lexical)/MyCompiler/MyCompiler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MyCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StreamReader SR = new StreamReader("file.txt");
+            string inputStr = SR.ReadToEnd();
+            Console.WriteLine(inputStr);
+            inputStr = Regex.Replace(inputStr, @" +", " ");
+            // remove empty lines
+            // inputStr = Regex.Replace(inputStr, @"\n+", "\n");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(inputStr);
+            Console.WriteLine("------------------------");
+            Lexical lex = new Lexical();
+            lex.findTokens(inputStr);
+            lex.printTokenList();
+        }
+    }
+}
